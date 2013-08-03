1b47ebd467dddfeb946bd54b9bbdee4abee695b2
Jaison Justus <jaison.justus.lp@gmail.com>
2013-08-03 21:30:10 +0530

    Update Ambience script.
    Add Method to create Audio Buffer and Attach the buffer to sound
    source.

--- app/scripts/ambience.js
+++ app/scripts/ambience.js
@@ -18,8 +18,16 @@ var Ambience = TRON.Ambience = {
 	_context : null,
 
 	_audioSources : {
-		telephone : 'telephone',
-		systemactivated : 'systemactivated'
+		telephone : {
+			source : 'telephone',
+			pos : { x : 0, y : 0, z : -30 },
+			gain : 5
+		},
+		systemactivated : {
+			source : 'systemactivated',
+			pos : { x : 68, y : 0, z : 33 },
+			gain : 0.3
+		}
 	},
 
 	_sounds : {},
@@ -31,26 +39,32 @@ var Ambience = TRON.Ambience = {
 
 	_createSoundNodes : function()	{
 		for(var source in this._audioSources)	{
-			var tempSource = {};
+			var tempSource = {},
+					pos = this._audioSources[source].pos;
 
 			tempSource.source = this._context.createBufferSource();
 			tempSource.volume = this._context.createGain();
 			tempSource.panner = this._context.createPanner();
 
-			tempSource.volume.gain.value = 5;
+			tempSource.volume.gain.value = this._audioSources[source].gain;
 			tempSource.source.connect(tempSource.volume);
 			tempSource.volume.connect(tempSource.panner);
 			tempSource.panner.connect(this._context.destination);
 			tempSource.source.loop = true;
 
 			this._sounds[source] = tempSource;
-
-			this._getSound(this._audioSources[source], this._attachSound, this);
+			this.setAmbienceSounds(this._audioSources[source].source, pos.x, pos.y, pos.z);
+			this._getSound(this._audioSources[source].source, this._attachSound, this);
 		}
 	},
 
 	_attachSound : function()	{
-		console.log(arguments);
+		var that = this.context,
+				sound = that._sounds[this.identifier],
+				response = this.response;
+
+		sound.source.buffer = that._context.createBuffer(response, false);
+	  sound.source.start(that._context.currentTime);
 	},
 
 	_getSound : function(source, callback, context)	{
@@ -65,6 +79,12 @@ var Ambience = TRON.Ambience = {
 		XHR.identifier = source;
 		XHR.onload = callback;
 		XHR.send();
+	},
+
+	setAmbienceSounds : function(identifier, x, y, z)	{
+		this._sounds[identifier].x = x;
+		this._sounds[identifier].y = y;
+		this._sounds[identifier].z = z;
 	}
 
 };
